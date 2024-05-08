5244bccdb95c9291e4ae0e660db8ad07fdd291a3
Dasha_Bolushevskaya <ddashalisinchuk@gmail.com>
2024-05-08 11:56:44 +0300

    исправила работу ссылок

--- src/my.py
+++ src/my.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/test2')
+def test2():
+    return render_template('test2.html')
+
+@app.route('/test3')
+def test3():
+    return render_template('test3.html')
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
